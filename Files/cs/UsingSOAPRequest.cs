732b68c44be0a16771219903fd4da1a1721875e6
janakiram775 <janakiram775@gmail.com>
2020-09-11 12:14:12 +0530

    Certification push

--- Files/cs/UsingSOAPRequest.cs
+++ Files/cs/UsingSOAPRequest.cs
@@ -18,7 +18,7 @@ namespace CertificationClio.Files.cs
 	{
        public override void OnSaving(object sender, EntityBeforeEventArgs e)
         {
-            //Calling CreateSOAPWebRequest method test 
+            //Calling CreateSOAPWebRequest method test1
             HttpWebRequest request = CreateSOAPWebRequest();
 
             string val = "";
